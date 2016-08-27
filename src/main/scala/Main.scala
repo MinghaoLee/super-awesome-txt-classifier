22795d4361cd3c5a92d0a6d64e1b5ce5f90fa28a
brad <bwbazemore@uga.edu>
2016-08-27 16:47:33 -0400

    removes stop words and everyhting to lower case

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -11,65 +11,82 @@ object Main {
 
   /**
     * Driver method
+    *
     * @param args commandline argument to driver
     */
-  def main(args: Array[String]): Unit ={
+  def main(args: Array[String]): Unit = {
 
     val conf = new SparkConf().setAppName("preprocessor")
     val sc = new SparkContext(conf)
 
     val trainData = sc.textFile("/home/brad/Documents/InteliJProjects/super-awesome-txt-classifier/X_train_vsmall.txt")
+    val stopWords = sc.textFile("/home/brad/Documents/InteliJProjects/super-awesome-txt-classifier/stopwords.txt")
+
+    val stopWordsSet = stopWords.collect.toSet
+    val stopWordsBC = sc.broadcast(stopWordsSet)
+
     val processedTrainData = trainData
-      .flatMap(word=>word.split(" "))
+      .flatMap(word => word.split(" "))
       .filter(PreprocessFunctions.removeNumbers)
       .map(PreprocessFunctions.removeSpecials)
       .map(PreprocessFunctions.removeForwardSlash)
       .map(PreprocessFunctions.removePeriod)
+      .map(word=>word.toLowerCase())
 
-    processedTrainData.take(100).foreach(println)
+    val cleanTrainData = processedTrainData.mapPartitions {
+      partition =>
+        val stopWordsSet = stopWordsBC.value
+        partition.filter(word => !stopWordsSet.contains(word))
+    }
+
+    cleanTrainData.take(100).foreach(println)
   }
 }
 
 /**
   * All functions related to preprocessing of data
   */
-object PreprocessFunctions{
+object PreprocessFunctions {
 
   /**
     * Function to be used on filters to remove all numbers and
     * strings with numbers
+    *
     * @param word instance of string from filter
     * @return
     */
-  def removeNumbers(word:String):Boolean={
+  def removeNumbers(word: String): Boolean = {
     word.matches("[^0-9]*")
   }
 
   /**
     * Removes the substring of &[a-z-A-Z]*;
+    *
     * @param word instance of string from map
     * @return
     */
-  def removeSpecials(word:String):String={
-    word.replaceAll("&[a-zA-Z]*;","")
+  def removeSpecials(word: String): String = {
+    word.replaceAll("&[a-zA-Z]*;", "")
   }
 
   /**
     * Removes any forward slash and replaces it with a space
+    *
     * @param word instance of string from map
     * @return
     */
-  def removeForwardSlash(word:String):String={
-    word.replaceAll("\\/"," ")
+  def removeForwardSlash(word: String): String = {
+    word.replaceAll("\\/", " ")
   }
 
   /**
     * Removes punctuation from string
+    *
     * @param word instance of string from map
     * @return
     */
-  def removePeriod(word:String):String={
-    word.replaceAll("\\p{Punct}","")
+  def removePeriod(word: String): String = {
+    word.replaceAll("\\p{Punct}", "")
   }
 
 }
