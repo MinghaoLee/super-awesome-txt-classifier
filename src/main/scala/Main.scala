84c0c76b6fd5ba794dc5c4a8a0665625bb79ae40
Brent Lippert <brentcl@uga.edu>
2016-08-27 14:53:43 -0400

    Added example data to its own directory for use in testing

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -3,8 +3,8 @@ import org.apache.spark.rdd.RDD
 /**
   * Created by bradford_bazemore on 8/19/16.
   */
-val XFile = sc.textFile("X_train_vsmall.txt")
-val YFile = sc.textFile("Y_train_vsmall.txt")
+val XFile = sc.textFile("Examples/Data/X_train_vsmall.txt")
+val YFile = sc.textFile("Examples/Data/Y_train_vsmall.txt")
 
 //Place holders, will be replaced by preprocessing
 val Dict = XFile.flatMap(line=>line.split(" ")).map(word=>(word,1)).reduceByKey(_+_)
