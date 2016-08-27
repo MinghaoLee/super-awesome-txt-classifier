e89ba4e4099b892fccc80a7833f621905871fd9e
Brent Lippert <brentcl@uga.edu>
2016-08-27 15:20:17 -0400

    Moved vals and defs inside the Main object inorder to fix build error

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,25 +1,26 @@
 import org.apache.spark.rdd.RDD
 
 /**
-  * Created by bradford_bazemore on 8/19/16.
+  *
   */
-val XFile = sc.textFile("Examples/Data/X_train_vsmall.txt")
-val YFile = sc.textFile("Examples/Data/Y_train_vsmall.txt")
-
-//Place holders, will be replaced by preprocessing
-val Dict = XFile.flatMap(line=>line.split(" ")).map(word=>(word,1)).reduceByKey(_+_)
-val DictTotal = Dict.count
-
-def WordProbabiliy (Dic: RDD[Pair[String, Int]], word: String) : Double {
-  var filtered = Dic.filter( (w: String, _) => word == w).first()
-  return (Double)filtered(1)/(Double)DictTotal
-}
-
-
-
 object Main {
+
+  def WordProbabiliy (Dic: RDD[Pair[String, Int]], word: String) : Double {
+    var filtered = Dic.filter( (w: String, _) => word == w).first()
+    return (Double)filtered(1)/(Double)DictTotal
+  }
+
+  val XFile = sc.textFile("Examples/Data/X_train_vsmall.txt")
+  val YFile = sc.textFile("Examples/Data/Y_train_vsmall.txt")
+
+  //Place holders, will be replaced by preprocessing
+  val Dict = XFile.flatMap(line=>line.split(" ")).map(word=>(word,1)).reduceByKey(_+_)
+  val DictTotal = Dict.count
+
   def main(args: Array[String]): Unit ={
   println(WordProbability(Dict, "the"))
+
+
 
   }
 }
