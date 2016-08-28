b2d80b54e56540a5645244cdd795c8cd3da11de0
Brent Lippert <brentcl@uga.edu>
2016-08-28 17:53:40 -0400

    Changed method names to follow scala conventions

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -9,33 +9,39 @@ object Main {
   val conf = new SparkConf().setAppName("preprocessor")
   val sc = new SparkContext(conf)
 
-  val xFile = sc.textFile("super-awesome-txt-classifier/Examples/Data/X_train_vsmall.txt")
-  val yFile = sc.textFile("super-awesome-txt-classifier/Examples/Data/Y_train_vsmall.txt")
+  val xTrainFile = sc.textFile("super-awesome-txt-classifier/Examples/Data/X_train_vsmall.txt")
+  val yTrainFile = sc.textFile("super-awesome-txt-classifier/Examples/Data/Y_train_vsmall.txt")
 
   //Place holders, will be replaced by preprocessing
-  val dict1 = xFile.flatMap(line=>line.split(" ")).map(word=>(word,1)).reduceByKey(_+_)
+  val dict1 = xTrainFile.flatMap(line=>line.split(" ")).map(word=>(word,1)).reduceByKey(_+_)
   val dictTotal1 = dict1.count
-  val dict2 = xFile.flatMap(line=>line.split(" ")).map(word=>(word,1)).reduceByKey(_+_)
+  val dict2 = xTrainFile.flatMap(line=>line.split(" ")).map(word=>(word,1)).reduceByKey(_+_)
   val dictTotal2 = dict1.count
   val dictProp1 = dictTotal1.toDouble / (dictTotal1 + dictTotal2)
   val dictProp2 = dictTotal2.toDouble / (dictTotal1 + dictTotal2)
 
-  def WordProbability (dic: RDD[Pair[String, Int]], word: String, dicTotal:Long) : Double = {
+  def wordProbability (dic: RDD[Pair[String, Int]], word: String, dicTotal:Long) : Double = {
     val filtered = dic.filter( (p:Pair[String,Int]) => word == p._1).first()
     return filtered._1.toDouble / dicTotal
   }
 
-  def ClassProbability(classDic: RDD[Pair[String,Int]], classDicTotal:Long, classProportion:Double, targetDic: RDD[Pair[String,Int]]) : Double = {
+  def classProbability(classDic: RDD[Pair[String,Int]], classDicTotal:Long, classProportion:Double, targetDic: RDD[Pair[String,Int]]) : Double = {
     var probability = 0.0
     targetDic.foreach( pair=>probability = probability + ( pair._2 * WordProbability(classDic, pair._1, classDicTotal)) )
     return probability * classProportion
   }
 
-  def Classify(targetDic: RDD[Pair[String,Int]]) : String = {
-    val probability1 = ClassProbability(dict1, dictTotal1, dictProp1, targetDic)
-    val probability2 = ClassProbability(dict2, dictTotal2, dictProp2, targetDic)
+  def classify(targetDic: RDD[Pair[String,Int]]) : String = {
+    val probability1 = classProbability(dict1, dictTotal1, dictProp1, targetDic)
+    val probability2 = classProbability(dict2, dictTotal2, dictProp2, targetDic)
     if (math.max(probability1, probability2) == probability1) return "Class1"
     else return "Class2"
+  }
+
+  def test(fileName:String): Unit = {
+    val xTestFile = sc.textFile(fileName)
+    val newFileName = "y" + fileName.drop(1)
+
   }
 
   def main(args: Array[String]): Unit ={
